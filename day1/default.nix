c9ddb40360c78e00312e68cc9b88283837b06cca
Jared Weakly <jaredweakly@gmail.com>
2019-12-04 17:46:54 -0800

    Clean up day1 slightly

--- day1/default.nix
+++ day1/default.nix
@@ -4,14 +4,18 @@ with builtins;
 let
   floor = n: bitOr n 0;
 
-  fuelRequired = mass: (floor (mass / 3)) - 2;
-
   sum = xs: foldl' (a: b: a + b) 0 xs;
 
+  # toInt is a delightfully terrible thing. It uses toJSON internally and
+  # aborts if it fails
   rd = p:
     map strings.toInt
     (filter (x: !(isList x) && (x != "")) (split "\n" (readFile p)));
 
+  # Answer to part 1
+  fuelRequired = mass: (floor (mass / 3)) - 2;
+
+  # Answer to part 2
   fuelRequired' = n:
     let n' = fuelRequired n;
     in if n' <= 0 then 0 else n' + (fuelRequired' n');
