006f2f3bb733948fc05b190c416ae52091f774c9
jared-w <jaredweakly@gmail.com>
2019-12-10 15:00:44 -0800

    State machine for day 4 part 2

--- day4/default.nix
+++ day4/default.nix
@@ -2,20 +2,41 @@
 with builtins;
 with pkgs.lib;
 with rec {
-  input = "146810-612564";
-  min = toInt (substring 0 6 input);
-  max = toInt (substring 7 6 input);
-  possibles = map toString (range min max);
+  possibles = map toString (range 146810 612564);
 
-  iterPairs = f: s: init (imap0 f (stringToCharacters s));
-  # hasAdjacent = s: any id (iterPairs (i: c: c == substring (i+1) 1 s) s);
-  hasAdjacent = s: length (filter isList (split "11[2-90]?|22|33|44|55|66|77|88|99|00" s)) > 0;
-  removeTriples = s: concatStrings (map (x: if !isList x then x else "-") (split "1{3,}|2{3,}|3{3,}|4{3,}|5{3,}|6{3,}|7{3,}|8{3,}|9{3,}|0{3,}" s));
-  allAscending = s: all id (iterPairs (i: c: c <= substring (i+1) 1 s) s);
+  initialState = {
+    char = "";
+    ascending = true;
+    adjacent = [ ];
+    valid = false;
+    any = false;
+    exact = false;
+  };
+
+  transition = state: c: rec {
+    char = c;
+    ascending = state.ascending && c >= state.char;
+
+    adjacent = [ c ] ++ optionals (c == state.char) state.adjacent;
+
+    valid = exact || length adjacent == 2;
+    # NB: Technically the "greater-than" here is unnecessary and just == is sufficient.
+    any = state.any || length adjacent >= 2;
+
+    # Without this separately from "valid", some corner cases will crop up:
+    # 1233444 <- should be valid, but because the 4444 is last there's no nice
+    #            way to track "have already seen a match of exactly two"
+    # 1234566 <- shjould be valid, but if you attempt to check if the third
+    #            item is different than the two, it'll fail because there's no
+    #            third item at the end of a list
+    exact = state.exact || (c != state.char && length state.adjacent == 2);
+  };
+
+  list =
+    map (foldl' transition initialState) (map stringToCharacters possibles);
 };
 
 {
-  inherit allAscending hasAdjacent removeTriples strings iterPairs;
-  one = count (s: hasAdjacent s && allAscending s) possibles;
-  # two = count (s: hasAdjacent s && allAscending s) (map removeTriples possibles);
+  one = count (s: s.any && s.ascending) list;
+  two = count (a: a.valid && a.ascending) list;
 }
